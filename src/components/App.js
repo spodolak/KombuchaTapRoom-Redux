cc719c9feb7dcfca83700d70ffb060adccd83684
Student <example@example.com>
2020-04-24 18:28:12 -0700

    debug babel-eslint versioning conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,7 @@ import React from "react";
 function App(){
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
-      <h3>3a</h3>
-      <h3>Thato and Haley</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr/>
+      <Header />
     </React.Fragment>
   );
 }

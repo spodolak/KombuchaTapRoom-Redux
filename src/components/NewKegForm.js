fb35f7c0ec90649e9846d77c6161c8fe97343b79
Student <example@example.com>
2020-04-25 15:08:14 -0700

    add form fields to NewKegForm.js

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,9 +1,37 @@
 import React from "react";
-
+import { v4 } from 'uuid';
+ 
 function NewKegForm(props) {
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.brand.value);
+    console.log(event.target.price.value);
+    console.log(event.target.flavor.value);
+  }
+
   return(
     <React.Fragment>
-      <h3>This is a form</h3>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Brew Name' />
+        <input
+          type='text'
+          name='brand'
+          placeholder='Brand' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Price Per Pint' />
+        <input
+          type='text'
+          name='flavor'
+          placeholder='Flavor' />
+        <button type='submit'>Add Keg</button>
+      </form>
     </React.Fragment>
   );
 }

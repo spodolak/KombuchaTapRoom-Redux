44b8d7bb87edf7c49e5487349ac834a0054367eb
Stephanie Podolak <StephPodolak@gmail.com>
2020-04-28 14:07:56 -0700

    update header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,7 +4,7 @@ function Header() {
 	return (
 		<React.Fragment>
 			<h1>Kombucha Tap Room</h1>
-			<p>A safe space where you don't have to worry about gluten allergies or autoimmune attacks</p>
+			<p>A safe space where you don't have to worry about gluten allergies</p>
 		</React.Fragment>
 	);
 }

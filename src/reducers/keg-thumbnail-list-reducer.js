7b174b96a17378fd0e70193cd0f4d85d364caa03
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-01 13:58:40 -0700

    write test for reducer. should successfully add a new keg to masterKegList. test passes

--- src/reducers/keg-thumbnail-list-reducer.js
+++ src/reducers/keg-thumbnail-list-reducer.js
@@ -1,3 +1,17 @@
 export default (state = {}, action) => {
-  return state;
+  switch (action.type) {
+  case 'ADD_KEG':
+    const { name, brand, price, flavor, id } = action;
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        flavor: flavor,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }
 };

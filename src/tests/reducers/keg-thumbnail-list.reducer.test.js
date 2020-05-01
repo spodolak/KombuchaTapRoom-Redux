203470ef00cf8da4c8ed05d1d7464b90426158f9
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-01 13:53:37 -0700

    write test for reducer. should successfully add a new keg to masterKegList. test fails

--- src/tests/reducers/keg-thumbnail-list.reducer.test.js
+++ src/tests/reducers/keg-thumbnail-list.reducer.test.js
@@ -1,7 +1,40 @@
 import kegThumbnailListReducer from '../../reducers/keg-thumbnail-list-reducer';
 
 describe('kegThumbnailListReducer', () => {
+
+  let action;
+  const kegData = {
+    name: "Strawberry",
+    brand: "Topsy Turvy",
+    price: "$.99",
+    flavor: "Strawberry",
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegThumbnailListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new keg to masterKegList', () => {
+    const { name, brand, price, flavor, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      flavor: flavor,
+      id: id
+    };
+
+    expect(kegThumbnailListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        flavor: flavor,
+        id: id
+      }
+    });
+  });
+
 });

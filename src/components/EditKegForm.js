147c586a99c8adf1e31a214d5941d353013b7949
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-01 10:55:29 -0700

    debug edit keg form

--- src/components/EditKegForm.js
+++ src/components/EditKegForm.js
@@ -4,11 +4,9 @@ import PropTypes from "prop-types";
 
 function EditKegForm (props) {
 
-  const { keg } = props;
-
   function handleEditKegFormSubmission(event) {
     event.preventDefault();
-    props.onEditKeg({name: event.target.name.value, brand: event.target.brand, price: event.target.price, flavor: event.target.flavor, id: keg.id})
+    props.onEditKeg({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, flavor: event.target.flavor.value, id: props.id})
   }
 
   return (
